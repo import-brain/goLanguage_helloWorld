142bb707a7e8f2adc84c1539a00d99d7471cf0c1
ericcheng2005 <68196762+import-brain@users.noreply.github.com>
2021-06-20 13:50:30 -0400

    func
    messing around with functions

--- over9000.go
+++ over9000.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var power int
-	power = 9000
+	power := 9000
 	fmt.Printf("It's over %d\n", power)
 }
---
+++ over9000_multiplefunctions.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+		power := getPower()
+		fmt.Printf("It's over %d\n", power)
+	}
+	func getPower() int {
+		return 9000
+	}
